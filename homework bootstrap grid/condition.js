020d4d08463150d919f1556c039f993cb9abcf90
AKash20dec <91383457+AKash20dec@users.noreply.github.com>
2023-07-01 13:37:52 +0530

    web page using bootstrap

---
+++ homework bootstrap grid/condition.js
@@ -0,0 +1,15 @@
+a=123;
+if(a%2==0){
+    console.log("number is even");
+}
+ else   {
+    myvar="nice";
+    var myVar1="using var";
+    // let myLet="using let";
+    const myvar3=123;
+            console.log("number is odd");
+ }
+console.log(myvar);
+console.log(myVar1);
+// console.log(myLet);
+// console.log(myvar3);
---
+++ homework bootstrap grid/index.js
@@ -0,0 +1,7 @@
+console.log("hello akash");
+
+num=8123;
+let myname="akash";
+console.log(num);
+console.log(myname);
+console.log(typeof myname);
